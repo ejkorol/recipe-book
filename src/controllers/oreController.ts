80d565e6aedb750be5476bd3fd4aa64e30c9574b
Jason Korol <ejkorol@gmail.com>
2024-07-20 23:35:27 -0400

    add functionality to post ore

--- src/controllers/oreController.ts
+++ src/controllers/oreController.ts
@@ -1,21 +1,29 @@
 import { Request, Response } from "express";
 import { OreSchema } from '../schemas/oreSchema';
-import { getAllOres as getAllOresService } from "../services/oreService";
+
+import {
+  getAllOres as getAllOresService,
+  postOre as postOreService
+} from "../services/oreService";
 
 export const getAllOres = async (_req: Request, res: Response) => {
   try {
     const ores = await getAllOresService();
     res.json(ores);
   } catch (e: any) {
-    res.status(500).json({ error: e.errors });
+    res.status(500).json({ error: e.message });
   };
 };
 
 export const postOre = async (req: Request, res: Response) => {
   try {
-    const ore = OreSchema.parse(req.body);
+    const oreData = OreSchema.parse(req.body);
+
+    const ore = await postOreService(oreData);
     res.json(ore);
+
   } catch (e: any) {
-    res.status(400).json({ error: e.errors });
+    console.error('Error posting ore:', e);
+    res.status(400).json({ error: e.errors || e.message });
   };
 };
