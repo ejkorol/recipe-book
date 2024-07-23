4659178169416e0f43f6b52db78554b4a4863689
Jason Korol <ejkorol@gmail.com>
2024-07-23 00:55:28 -0400

    add new errors for validation issues

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -4,34 +4,41 @@ export class ApiError extends Error {
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    this.name = "ApiError";
+    this.name = "Api Error";
   }
 }
 
 export class NotFoundError extends ApiError {
   constructor(message: string = "Not Found") {
     super(message, 404);
-    this.name = "NotFoundError";
+    this.name = "Not Found Error";
   }
 }
 
 export class DatabaseError extends ApiError {
   constructor(message: string = "Database Error") {
     super(message, 500);
-    this.name = "DatabaseError";
+    this.name = "Database Error";
   }
 }
 
 export class UnknownError extends ApiError {
   constructor(message: string = "Unknown Error") {
     super(message, 500);
-    this.name = "UnknownError";
+    this.name = "Unknown Error";
   }
 }
 
 export class ValidationError extends ApiError {
   constructor(message: string = "Validation Error") {
     super(message, 400);
-    this.name = "ValidationError";
+    this.name = "Validation Error";
+  }
+}
+
+export class MissingParameterError extends ApiError {
+  constructor(message: string = "Missing Parameter Error") {
+    super(message, 400);
+    this.name = "Missing Paramaters"
   }
 }
