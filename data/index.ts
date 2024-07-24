9faa0d3ddda7fa861647030862d4bc81d21a9f4d
Jason Korol <ejkorol@gmail.com>
2024-07-24 00:12:55 -0400

    deprecating unsupported version

--- data/index.ts
+++ data/index.ts
@@ -9,17 +9,6 @@ const data = {
       materials: "./data/1.19/materials.json",
       recipes: "./data/1.19/recipes.json" 
     }
-  },
-  "1.20.2": {
-    files: {
-      blocks: "./data/1.20.2/blocks.json",
-      biomes: "./data/1.20.2/biomes.json",
-      foods: "./data/1.20.2/foods.json",
-      entities: "./data/1.20.2/entities.json",
-      items: "./data/1.20.2/items.json",
-      materials: "./data/1.20.2/materials.json",
-      recipes: "./data/1.20.2/recipes.json"
-    }
   }
 };
 
--- src/seed/scripts/1.20.2/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./biomes";
-export * from "./blocks";
-export * from "./entities";
-export * from "./foods";
-export * from "./items";
-export * from "./materials";
-export * from "./recipes";
