7b17eb9c6bde1c8231f321065000281aff0c199f
Jason Korol <ejkorol@gmail.com>
2024-07-25 13:03:03 -0400

    test migration of api to index

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,56 @@
-import api from "../src/api";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+/* RATE LIMITER */
+import { apiLimiter } from '../src/middleware/rateLimiter';
+
+/* ROUTES */
+import routes from "../src/routes"
+
+/* MIDDLEWARE */
+import { errorHandler } from '../src/middleware/errorHandler';
+
+/* ********************************** */
+/*              ENV CONFIG            */
+/* ********************************** */
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+/* CONFIG */
+const api = express();
+
+/* ********************************** */
+/*              MIDDLEWARE            */
+/* ********************************** */
+
+/* CORS W/ DEFAULT */
+api.use(cors({ origin: '*' }));
+
+/* ENABLE RATE LIMITER IN PROD */
+api.set('trust proxy', true);
+
+if (process.env.NODE_ENV === 'production') {
+  api.use(apiLimiter);
+};
+
+api.use(express.json());
+api.use(express.static('public'));
+api.use(routes);
+
+/* ********************************** */
+/*           ERROR HANDLER            */
+/* ********************************** */
+api.use(errorHandler);
+
+/*
+ * Error handler middleware must be declared after all routes
+ * to allow it to catch errors thrown from any route.
+*/
+
+/* SERVE ~ */
+api.listen(PORT, () => {
+  console.log(`API is listening on port: ${PORT}`);
+});
 
 export default api;
